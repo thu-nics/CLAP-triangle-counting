ff6219ac5099978c37d378144844020f9cad563b
dubcy43 <1780277455@qq.com>
2022-11-13 16:44:16 +0000

    edit shell script of CAM simulator

--- CAM_simulator/run.sh
+++ CAM_simulator/run.sh
@@ -1,15 +1,15 @@
 RUN_EXE="./build/CAM_architecture_simulation"
-DATA_FOLDER1="../data/CSR/random"
-DATA_FOLDER2="../data/CSR/force_test"
+DATA_FOLDER1="../data/CSR/random_order"
+DATA_FOLDER2="../data/CSR/force_order"
 
 DATA_FOLDER=$DATA_FOLDER2
 OUTPUT_FOLDER="../output"
-TRACE_FOLDER="../output/trace/force_test"
+TRACE_FOLDER="../output/trace/force_order"
 
-OUTFILE="force_test"
+OUTFILE="force_order"
 mkdir $OUTPUT_FOLDER
 mkdir $TRACE_FOLDER
-for GRAPH in "citeseer" #"p2p"  "mico" "patent" "youtube" "astro" "email-Enron" "roadNet-PA" "roadNet-TX" "lj" #
+for GRAPH in "citeseer" 
 do
     mkdir $TRACE_FOLDER/$GRAPH
     $RUN_EXE $DATA_FOLDER/$GRAPH.bin 1 1 $TRACE_FOLDER/$GRAPH/$GRAPH >> $OUTPUT_FOLDER/$OUTFILE.stdout
